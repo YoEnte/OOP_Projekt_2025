9ab763d7cd0bea7b67c3e6663f24cfcccc4a5cdc
Tomek <tho.kacza@gmail.com>
2025-06-12 20:43:51 +0200

    Enemies refactor, add AbstractEnemy, HardEnemy, NormalEnemy, EasyEnemy

---
+++ src/LabyrinthPerson_v3/model/enemy/EasyEnemy.java
@@ -0,0 +1,10 @@
+package model.enemy;
+
+public class EasyEnemy extends AbstractEnemy{
+
+    public EasyEnemy(int positionX,int positionY){
+        super(positionX, positionY);
+
+
+    }
+}
---
+++ src/LabyrinthPerson_v3/model/enemy/HardEnemy.java
@@ -0,0 +1,10 @@
+package model.enemy;
+
+public class HardEnemy extends AbstractEnemy {
+
+    public HardEnemy(int positionX,int positionY){
+        super(positionX, positionY);
+
+
+    }
+}
